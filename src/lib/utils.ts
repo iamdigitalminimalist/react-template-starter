b1039d77295283287a2c65261c89fd71bc8d3ee3
or-druker <hello+github@druker.net>
2024-12-11 21:32:22 +0200

    Add util function isEmpty

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,23 @@ export function toTitleCase(str: string): string {
     (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
   );
 }
+
+export function isEmpty(value: unknown): boolean {
+  if (value === null || value === undefined) {
+    return true;
+  }
+
+  if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length === 0;
+  }
+
+  if (Array.isArray(value) && value.length === 0) {
+    return true;
+  }
+
+  if (typeof value === 'object') {
+    return Object.keys(value).length === 0;
+  }
+
+  return false;
+}
