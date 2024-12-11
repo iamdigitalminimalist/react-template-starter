0d0c204d36d902e1a5d6709f8d9bcbf129200fc9
or-druker <hello+github@druker.net>
2024-12-11 18:33:49 +0200

    Add mock of dom api to vitest setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/vitest';
+
+// Mocking `scrollIntoView` to prevent TypeError during tests.
+// Mocking pointer capture methods to address "target.hasPointerCapture is not a function" errors.
+// Reference: https://github.com/testing-library/user-event/discussions/1087
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
