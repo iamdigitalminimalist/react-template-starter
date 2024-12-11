5ec907dccc7f31bc486db5bb8603253eb241e49e
or-druker <hello+github@druker.net>
2024-12-11 17:48:25 +0200

    Remove toaster context provider from main

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import PlayGroundPage from './pages/PlayGroundPage.tsx';
-import { Toaster } from './components/ui/toaster.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Toaster />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="playground" element={<PlayGroundPage />} />
