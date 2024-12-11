1c88d3076aeddeaa6da1965a5d3c69df2e08ff16
or-druker <hello+github@druker.net>
2024-12-11 15:02:42 +0200

    Exclude directory from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.{ts,tsx}', 'tests/**/*.spec.{ts,tsx}'],
+    exclude: [
+      'tests/excluded/**/*.test.{ts,tsx}',
+      'tests/excluded/**/*.spec.{ts,tsx}',
+    ],
     environment: 'jsdom',
     globals: true,
     setupFiles: 'tests/setup.ts',
